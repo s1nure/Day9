741011bd1dc486226a91ed966035d63b3ecd4260
Volodumur Ivanov <s1nure000@gmail.com>
2023-10-26 15:57:07 +0200

    first commit

---
+++ src/components/StudentsComponent.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <table class="w-full border-3 text-left text- bg-slate-300">
+      <thead class="border-b">
+        <tr>
+          <th>Name</th>
+          <th>group</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="student, index in students" :key="index">
+          <td>{{ student.name }}</td>
+          <td>{{ student.group }}</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+import axios from 'axios'
+const students = ref([])
+
+onMounted(()=>{
+  axios
+  .get('http://34.82.81.113:3000/students')
+  .then((response)=>{
+    students.value = response.data
+  })
+})
+</script >
---
+++ src/views/HomeView.vue
@@ -0,0 +1,9 @@
+<script setup>
+import StudentsComponent from '../components/StudentsComponent.vue'
+</script>
+
+<template>
+  <main>
+    <StudentsComponent />
+  </main>
+</template>
